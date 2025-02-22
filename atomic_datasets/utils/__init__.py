6d5bdb844fff676e93452b7b6e7ce53a1fc0f967
Ameya Daigavane <ameya.d.98@gmail.com>
2025-02-22 12:31:51 -0500

    Add atom symbol and number tools.

--- atomic_datasets/utils/__init__.py
+++ atomic_datasets/utils/__init__.py
@@ -1,4 +1,4 @@
 from .ase import ase_atoms_to_graph
-from .rdkit import is_molecule_sane
+from .rdkit import is_molecule_sane, atom_number_to_symbol, atom_symbol_to_number
 from .download import clone_url, download_url, extract_zip, extract_gz, extract_tar
 from .decorators import after_preprocess
